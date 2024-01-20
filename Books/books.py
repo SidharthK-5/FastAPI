bb70c18cbf29e5bdfa2b154a32166cdfaed52baf
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-01-20 13:41:54 +0530

    feat: - books2 app - code formatting

--- Books/books.py
+++ Books/books.py
@@ -3,15 +3,16 @@ from fastapi import Body, FastAPI
 app = FastAPI()
 
 BOOKS = [
-    {"title": "Title one", "author": "Author one", "category": 'science'},
-    {"title": "Title two", "author": "Author two", "category": 'science'},
-    {"title": "Title three", "author": "Author three", "category": 'history'},
-    {"title": "Title four", "author": "Author four", "category": 'math'},
-    {"title": "Title five", "author": "Author five", "category": 'math'},
-    {"title": "Title six", "author": "Author two", "category": 'math'},
+    {"title": "Title one", "author": "Author one", "category": "science"},
+    {"title": "Title two", "author": "Author two", "category": "science"},
+    {"title": "Title three", "author": "Author three", "category": "history"},
+    {"title": "Title four", "author": "Author four", "category": "math"},
+    {"title": "Title five", "author": "Author five", "category": "math"},
+    {"title": "Title six", "author": "Author two", "category": "math"},
 ]
 
 # GET Requests
+
 
 @app.get("/")
 async def home():
@@ -28,47 +29,57 @@ async def first_api():
 async def read_all_books():
     return BOOKS
 
+
 # URL with Path parameter 1
 @app.get("/books/id/{book_id}")
 async def read_book_by_id(book_id: int):
-    return BOOKS[book_id-1]
+    return BOOKS[book_id - 1]
+
 
 # URL with Path parameter 2
 @app.get("/books/title/{book_title}")
 async def read_book_by_title(book_title: str):
     for book in BOOKS:
-        if book.get('title').casefold() == book_title.casefold():
+        if book.get("title").casefold() == book_title.casefold():
             return book
-        
+
+
 # URL with query parameter
 @app.get("/books/category")
 async def read_book_by_category(category: str) -> list[dict]:
     books_to_return = []
     for book in BOOKS:
-        if book.get('category').casefold() == category.casefold():
+        if book.get("category").casefold() == category.casefold():
             books_to_return.append(book)
     return books_to_return
+
 
 @app.get("/books/author")
 async def read_all_book_by_author(book_author: str) -> list[dict]:
     books_to_return = []
     for book in BOOKS:
-        if book.get('author').casefold() == book_author.casefold():
+        if book.get("author").casefold() == book_author.casefold():
             books_to_return.append(book)
     return books_to_return
 
+
 # URL with path and query parameter
 @app.get("/books/author-and-category/{book_author}")
-async def read_book_by_author_and_category(book_author: str, category: str) -> list[dict]:
+async def read_book_by_author_and_category(
+    book_author: str, category: str
+) -> list[dict]:
     books_to_return = []
     for book in BOOKS:
-        if book.get('author').casefold() == book_author.casefold() and \
-                book.get('category').casefold() == category.casefold():
+        if (
+            book.get("author").casefold() == book_author.casefold()
+            and book.get("category").casefold() == category.casefold()
+        ):
             books_to_return.append(book)
     return books_to_return
 
 
 # POST Requests
+
 
 @app.post("/books/create-book")
 async def create_book(new_book=Body()) -> dict:
@@ -78,20 +89,22 @@ async def create_book(new_book=Body()) -> dict:
 
 # PUT Requests
 
+
 @app.put("/books/update-book")
 async def update_book_by_title(updated_book=Body()) -> dict:
     for idx in range(len(BOOKS)):
-        if BOOKS[idx].get('title').casefold() == updated_book.get('title').casefold():
+        if BOOKS[idx].get("title").casefold() == updated_book.get("title").casefold():
             BOOKS[idx] = updated_book
     return updated_book
 
 
 # DELETE Requests
 
+
 @app.delete("/books/delete-book/{book_title}")
 async def delete_book_by_title(book_title: str) -> dict:
     for idx in range(len(BOOKS)):
-        if BOOKS[idx].get('title').casefold() == book_title.casefold():
+        if BOOKS[idx].get("title").casefold() == book_title.casefold():
             deleted_book = BOOKS.pop(idx)
             break
     return deleted_book
---
+++ Books/books2.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating) -> None:
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
+BOOKS = [
+    Book(1, "Computer Science Architecture", "Novice Coder", "A very nice book!", 5),
+    Book(2, "Be Fast with FastAPI", "Your guide to FastAPI", "A great book!", 5),
+    Book(3, "Master Endpoints", "Youlearn", "A awesome book!", 5),
+    Book(4, "HP1", "Author 1", "Book Description", 2),
+    Book(5, "HP2", "Author 2", "Book Description", 3),
+    Book(6, "HP3", "Author 3", "Book Description", 1),
+]
+
+
+@app.get("/")
+async def home():
+    return {"message": "Health Check Successful"}
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
+    return book_request

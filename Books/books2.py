ac51c475404455bcacb6ce98c67dc16ff36e5913
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-01-21 17:35:11 +0530

    feat: - data validation and example schema with pudantic - update and delete apis

--- Books/books2.py
+++ Books/books2.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -11,30 +12,47 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating) -> None:
+    def __init__(self, id, title, author, description, rating, published_date) -> None:
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
+
+    # class Config is a pydantic class which will help setting up a sample schema in Swagger UI
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "A new book",
+                "author": "New author",
+                "description": "Description for new book",
+                "rating": 5,
+                "published_date": 2024,
+            }
+        }
 
 
 BOOKS = [
-    Book(1, "Computer Science Architecture", "Novice Coder", "A very nice book!", 5),
-    Book(2, "Be Fast with FastAPI", "Your guide to FastAPI", "A great book!", 5),
-    Book(3, "Master Endpoints", "Youlearn", "A awesome book!", 5),
-    Book(4, "HP1", "Author 1", "Book Description", 2),
-    Book(5, "HP2", "Author 2", "Book Description", 3),
-    Book(6, "HP3", "Author 3", "Book Description", 1),
+    Book(
+        1, "Computer Science Architecture", "Novice Coder", "A very nice book!", 5, 2030
+    ),
+    Book(2, "Be Fast with FastAPI", "Your guide to FastAPI", "A great book!", 5, 2030),
+    Book(3, "Master Endpoints", "Youlearn", "A awesome book!", 5, 2029),
+    Book(4, "HP1", "Author 1", "Book Description", 2, 2028),
+    Book(5, "HP2", "Author 2", "Book Description", 3, 2027),
+    Book(6, "HP3", "Author 3", "Book Description", 1, 2026),
 ]
 
 
@@ -48,8 +66,56 @@ async def read_all_books():
     return BOOKS
 
 
+@app.get("/books/read/{book_id}")
+async def read_book_by_id(bookd_id: int):
+    for book in BOOKS:
+        if book.id == bookd_id:
+            return book
+
+
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/publish/")
+async def read_books_by_publish_date(published_date: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
+
+
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
+    new_book = find_book_id(new_book)
     BOOKS.append(new_book)
-    return book_request
+    return new_book
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
+
+
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+    return book
+
+
+@app.delete("/books/delete/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            deleted_book = BOOKS.pop(i)
+            break
+    return deleted_book

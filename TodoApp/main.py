9e069a09f756395ea68727a15d4a967e789a259f
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-03-03 16:33:18 +0530

    feat TodoApp: - new route User to view and change password of logged in user

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import auth, todos, admin
+from routers import auth, todos, admin, users
 
 app = FastAPI()
 
@@ -9,4 +9,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(router=auth.router, tags=["Authentication"])
 app.include_router(router=admin.router, tags=["Admin"])
+app.include_router(router=users.router, tags=["User"])
 app.include_router(router=todos.router, tags=["Manage Todos"])

a43f55e48efeb0d4e437324b5aaf5a93b535643c
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-05-04 21:53:04 +0530

    feat RandomName: - full stack application to fetch random name from lists - team_members list added to gitignore

---
+++ RandomName/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+# from starlette import status
+# from starlette.responses import RedirectResponse
+import random
+from team_members import DEV_TEAM, QA_TEAM, WHOLE_TEAM
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.get("/random-dev-team-member", response_class=HTMLResponse)
+async def random_dev_team_member():
+    return random.choice(DEV_TEAM)
+
+
+@app.get("/random-qa-team-member", response_class=HTMLResponse)
+async def random_qa_team_member():
+    return random.choice(QA_TEAM)
+
+
+@app.get("/random-team-member", response_class=HTMLResponse)
+async def random_team_member():
+    return random.choice(WHOLE_TEAM)
+
